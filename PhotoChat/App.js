d9f4bb6f65c670efd2943b1ff0368826c54de81c
ThibaMahlezana <thiba.ma@gmail.com>
2022-05-18 13:00:24 +0200

    Initializing project files

---
+++ PhotoChat/App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      {
+        <View style={styles.container}>
+          <Text>PhotoChat</Text>
+          <StatusBar style="auto" />
+        </View>
+      }
+    </NavigationContainer>
+  );
+  return (
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
