0f7e6b191cf17471e37d174330916c6072b26028
ThibaMahlezana <thiba.ma@gmail.com>
2022-05-19 15:53:44 +0200

    Log in screen frontend development

--- PhotoChat/screens/loginScreen.js
+++ PhotoChat/screens/loginScreen.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
 import theme from '../core/theme';
 
 export default function Login(){
     return(
       <View style={styles.container}>
           <Text style={styles.header}>sign in</Text>
-          <Text>PhotoChat</Text>
+          <View style={styles.input_area}>
+            <TextInput style={styles.input} placeholder="Email"/>
+            <TextInput style={styles.input} placeholder="Password"/>
+            <Pressable style={styles.button}>
+                <Text style={styles.text}>Sign in</Text>
+            </Pressable>
+            <Text style={styles.description}>Don't have an accout? sign up.</Text>
+          </View>
       </View>
     );
 }
@@ -18,9 +25,44 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   header: {
-    fontSize: 50,
+    fontSize: 30,
     fontFamily: 'Nunito',
     color: theme.PRIMARY_COLOR,
     textTransform: 'uppercase',
+    fontWeight: '800'
+  },
+  input_area : {
+    width: '80%'
+  },
+  input: {
+    height: 50,
+    marginVertical: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    borderColor: theme.PRIMARY_COLOR
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 10,
+    elevation: 3,
+    backgroundColor: theme.PRIMARY_COLOR,
+    marginTop: 12,
+    height: 50
+  },
+  text : {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+    textTransform: 'uppercase'
+  },
+  description: {
+    marginTop: 12,
+    color: '#606060',
   }
 });
--- PhotoChat/screens/registerScreen.js
+++ PhotoChat/screens/registerScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
 import theme from '../core/theme';
 
 export default function Register(){
